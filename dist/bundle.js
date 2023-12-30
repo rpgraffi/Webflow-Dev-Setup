6413a262fb3ae9e80b22af56698e68d3a1bf685b
Raphael Wennmacher <51893700+rpgraffi@users.noreply.github.com>
2023-12-30 20:09:19 +0000

    Update 2 files

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,2 +1,1 @@
 console.log("Flow Phantom");
-//# sourceMappingURL=bundle.js.map
---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  devtool: "inline-source-map",
+  mode: "development",
+  devServer: {
+    hot: false,
+    disableHostCheck: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
+});
